038c35aa9ebfc23ede6bd0147bc89b2f103d7bc9
Wil Thieme <wil.thieme@protonmail.com>
2020-07-24 17:25:21 -0400

    parse damage for energy
    energy does half damage before hardness except at GM discretion

---
+++ campaign/scripts/item_damage.lua
@@ -0,0 +1,73 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+function onInit()
+	onValueChanged()
+end
+function onValueChanged()
+	local nItemHitpoints = window.hitpoints.getValue()
+	if nItemHitpoints and nItemHitpoints >= 1 then
+		local nDmg = window.item_damage.getValue()
+		local nPercentDmg = nDmg / nItemHitpoints * 100
+		if nPercentDmg >= 100 then
+			window.item_damage.setColor('800D0D0D')
+		elseif nPercentDmg >= 66 then
+			window.item_damage.setColor('FFB22929')
+		elseif nPercentDmg >= 33 then
+			window.item_damage.setColor('FFEB7B00')
+		else
+			window.item_damage.setColor('FF000000')
+		end
+	end
+end
+
+---	This function converts CSVs from a string to a table of values
+--	@param s input, a string of CSVs
+--	@return t output, an indexed table of values
+function fromCSV(s)
+	local fieldstart = 1
+
+	s = s .. '☹'
+	local nPosition = string.find(s, 'TYPE: ', fieldstart)
+	local nStop = string.find(s, '☹', fieldstart)
+	s = string.sub(s, nPosition, nStop) -- trim off everything to "TYPE: "
+	nStop = string.find(s, ' %(', fieldstart)
+	s = string.sub(s, fieldstart, nStop) -- trim off everything to " ("
+
+	s = s .. ','        -- ending comma
+	local t = {}        -- table to collect fields
+	repeat
+		local nexti = string.find(s, ',', fieldstart)
+		table.insert(t, string.sub(s, fieldstart, nexti-1))
+		fieldstart = nexti + 1
+	until fieldstart > string.len(s)
+
+	return t
+end
+
+function onDrop(x, y, draginfo)
+	local nDmg = window.item_damage.getValue()
+	local nHardness = window.hardness.getValue()
+	local nDamageDealt = draginfo.getNumberData()
+	
+	local tEnergyTypes = {'fire','cold','acid','lightning','sonic'}
+	local tDmgType = fromCSV(draginfo.getDescription())
+
+	for _,v in ipairs(tDmgType) do
+		for _, vv in ipairs(tEnergyTypes) do
+			if string.match(v, vv) then
+				nDamageDealt = nDamageDealt / 2
+				break
+			end
+		end
+	end
+				
+	local nModifiedDamage = nDamageDealt - nHardness
+
+	if nModifiedDamage < 1 then
+		window.item_damage.setValue(nDmg)
+	else
+		window.item_damage.setValue(nDmg + nModifiedDamage)
+	end
+end
---
+++ campaign/scripts/item_main2.lua
@@ -0,0 +1,103 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onInit()
+	update()
+end
+
+function VisDataCleared()
+	update()
+end
+
+function InvisDataAdded()
+	update()
+end
+
+function updateControl(sControl, bReadOnly, bID)
+	if not self[sControl] then
+		return false
+	end
+		
+	if not bID then
+		return self[sControl].update(bReadOnly, true)
+	end
+	
+	return self[sControl].update(bReadOnly)
+end
+
+function update()
+	local nodeRecord = getDatabaseNode()
+	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord)
+	local bID = LibraryData.getIDState('item', nodeRecord)
+	
+	local sType = type.getValue()
+	local bWeapon = (sType == 'Weapon')
+	local bArmor = (sType == 'Armor')
+	
+	local bSection1 = false
+	if User.isHost() then
+		if updateControl('nonid_name', bReadOnly, true) then bSection1 = true end
+	else
+		updateControl('nonid_name', false)
+	end
+	if (User.isHost() or not bID) then
+		if updateControl('nonidentified', bReadOnly, true) then bSection1 = true end
+	else
+		updateControl('nonidentified', false)
+	end
+
+	local bSection2 = false
+	if updateControl('type', bReadOnly, bID) then bSection2 = true end
+	if updateControl('subtype', bReadOnly, bID) then bSection2 = true end
+	
+	local bSection2a = false
+	if updateControl('hardness', bReadOnly, bID) then bSection2a = true end
+	if updateControl('hitpoints', bReadOnly, bID) then bSection2a = true end
+	if updateControl('item_damage', bReadOnly, bID) then bSection2a = true end
+	if
+		updateControl('hardness', bReadOnly, bID)
+	or
+		updateControl('hitpoints', bReadOnly, bID)
+	then
+		item_damage.setVisible(true)
+	end
+	item_damage.setReadOnly(false)
+	
+	local bSection3 = false
+	if updateControl('cost', bReadOnly, bID) then bSection3 = true end
+	if updateControl('weight', bReadOnly, bID) then bSection3 = true end
+	
+	local bSection4 = false
+	if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true end
+	if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true end
+	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true end
+	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true end
+	
+	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true end
+	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true end
+	if updateControl('checkpenalty', bReadOnly, bID and bArmor) then bSection4 = true end
+	if updateControl('spellfailure', bReadOnly, bID and bArmor) then bSection4 = true end
+	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true end
+	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true end
+
+	if updateControl('properties', bReadOnly, bID and (bWeapon or bArmor)) then bSection4 = true end
+	
+	local bSection5 = false
+	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true end
+	if updateControl('aura', bReadOnly, bID) then bSection5 = true end
+	if updateControl('cl', bReadOnly, bID) then bSection5 = true end
+	if updateControl('prerequisites', bReadOnly, bID) then bSection5 = true end
+	
+	local bSection6 = bID
+	description.setVisible(bID)
+	description.setReadOnly(bReadOnly)
+	
+	divider.setVisible(bSection1 and bSection2)
+	divider2.setVisible((bSection1 or bSection2) and bSection2a)
+	divider2a.setVisible((bSection1 or bSection2 or bSection2a) and bSection3)
+	divider3.setVisible((bSection1 or bSection2 or bSection2a or bSection3) and bSection4)
+	divider4.setVisible((bSection1 or bSection2 or bSection2a or bSection3 or bSection4) and bSection5)
+	divider5.setVisible((bSection1 or bSection2 or bSection2a or bSection3 or bSection4 or bSection5) and bSection6)
+end
