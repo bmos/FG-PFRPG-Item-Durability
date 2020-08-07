b6f3563b47cdea94696cae05b77ee047ea1f4696
Wil Thieme <wil.thieme@protonmail.com>
2020-08-07 18:18:30 -0400

    change hitpoints per inch key

--- scripts/info_itemdurability.lua
+++ scripts/info_itemdurability.lua
@@ -9,66 +9,66 @@ function provideValues(sK)
 
 	if sK == 'materials' then
 		-- materials from Special Materials list
-		t['abysium'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['angelskin'] = { ['nHardness'] = 5, ['nHpIn'] = 5 }
-		t['aszite'] = { ['nHardness'] = 15, ['nHpIn'] = 20 }
-		t['magic bridge basalt'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }	-- not sure about this hp/in figure
-		t['blight quartz'] = { ['nHardness'] = 10, ['nHpIn'] = 10 }
-		t['bone'] = { ['nHardness'] = 5, ['nHpIn'] = 12 }			-- I made up this nHpIn
-	--	t['brass'] = { ['nHardness'] = 9, ['nHpIn'] = 0 }			-- This should exist, but doesn't
-		t['chitin'] = { ['nHardness'] = 5, ['nHpIn'] = 12 }		-- I made this up based on bone
-		t['coral'] = { ['nHardness'] = 5, ['nHpIn'] = 12 }		-- I made this up based on bone
-		t['cryptstone'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['blood crystal'] = { ['nHardness'] = 10, ['nHpIn'] = 10, ['nWeaponHpMult'] = 0.5 }
-		t['darkleaf cloth'] = { ['nHardness'] = 10, ['nHpIn'] = 20 }
-		t['darkwood'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }
-		t['dragonhide'] = { ['nHardness'] = 10, ['nHpIn'] = 10 }	-- hide of a dragon is typically between 1/2 inch and 1 inch thick
-		t['druchite'] = { ['nHardness'] = 10, ['nHpIn'] = 40 }
-		t['eel hide'] = { ['nHardness'] = 2, ['nHpIn'] = 5 }
-		t['elysian bronze'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-	--	t['bronze'] = { ['nHardness'] = 9, ['nHpIn'] = 10 }		-- bronze armor has hardness 9 and bronze weapons use the same hardness as their base weapon
-		t['gold'] = { ['nHardness'] = 5, ['nHpIn'] = 12 }			-- I made up this nHpIn
-		t['greenwood'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }
-		t['griffon mane'] = { ['nHardness'] = 1, ['nHpIn'] = 2 }
-		t['horacalcum'] = { ['nHardness'] = 15, ['nHpIn'] = 30, ['nArmorHpMult'] = 1.25, ['nWeaponHpMult'] = 1.25 }
-		t['inubrix'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }
-		t['cold iron'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['mindglass'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['noqual'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-	--	t['obsidian'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }	-- obsidian weapons have half the hardness of their base weapon 
-		t['siccatite'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }	-- I made this up based on iron/steel
-		t['alchemical silver'] = { ['nHardness'] = 8, ['nHpIn'] = 10 }	-- I made this up based on iron/steel
-		t['silversheen'] = { ['nHardness'] = 8, ['nHpIn'] = 10 }
-		t['glaucite'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['spiresteel'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['net'] = { ['nHardness'] = 2, ['nHpIn'] = 5 }			-- I made this up based on leather
-		t['living steel'] = { ['nHardness'] = 15, ['nHpIn'] = 35 }
-		t['singing steel'] = { ['nHardness'] = 10, ['nHpIn'] = 20 }
-		t['sunsilver'] = { ['nHardness'] = 8, ['nHpIn'] = 10 }
-		t['silver'] = { ['nHardness'] = 8, ['nHpIn'] = 10 }		-- This is a workaround because nobody says "alchemical silver" in their weapon names
-	--	t['viridium'] = { ['nHardness'] = 8, ['nHpIn'] = 10 }		-- viridium weapons have half the hardness of their base weapon 
-		t['voidglass'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['whipwood'] = { ['nHardness'] = 8, ['nHpIn'] = 10, ['nWeaponHpBonus'] = 5 }
-		t['voidglass'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['wyroot'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }
+		t['abysium'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['angelskin'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 5 }
+		t['aszite'] = { ['nHardness'] = 15, ['nItemHpPerIn'] = 20 }
+		t['magic bridge basalt'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }	-- not sure about this hp/in figure
+		t['blight quartz'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 10 }
+		t['bone'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 12 }			-- I made up this nItemHpPerIn
+	--	t['brass'] = { ['nHardness'] = 9, ['nItemHpPerIn'] = 0 }			-- This should exist, but doesn't
+		t['chitin'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 12 }		-- I made this up based on bone
+		t['coral'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 12 }		-- I made this up based on bone
+		t['cryptstone'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['blood crystal'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 10, ['nWeaponHpMult'] = 0.5 }
+		t['darkleaf cloth'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 20 }
+		t['darkwood'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }
+		t['dragonhide'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 10 }	-- hide of a dragon is typically between 1/2 inch and 1 inch thick
+		t['druchite'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 40 }
+		t['eel hide'] = { ['nHardness'] = 2, ['nItemHpPerIn'] = 5 }
+		t['elysian bronze'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+	--	t['bronze'] = { ['nHardness'] = 9, ['nItemHpPerIn'] = 10 }		-- bronze armor has hardness 9 and bronze weapons use the same hardness as their base weapon
+		t['gold'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 12 }			-- I made up this nItemHpPerIn
+		t['greenwood'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }
+		t['griffon mane'] = { ['nHardness'] = 1, ['nItemHpPerIn'] = 2 }
+		t['horacalcum'] = { ['nHardness'] = 15, ['nItemHpPerIn'] = 30, ['nArmorHpMult'] = 1.25, ['nWeaponHpMult'] = 1.25 }
+		t['inubrix'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }
+		t['cold iron'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['mindglass'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['noqual'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+	--	t['obsidian'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }	-- obsidian weapons have half the hardness of their base weapon 
+		t['siccatite'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }	-- I made this up based on iron/steel
+		t['alchemical silver'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10 }	-- I made this up based on iron/steel
+		t['silversheen'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10 }
+		t['glaucite'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['spiresteel'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['net'] = { ['nHardness'] = 2, ['nItemHpPerIn'] = 5 }			-- I made this up based on leather
+		t['living steel'] = { ['nHardness'] = 15, ['nItemHpPerIn'] = 35 }
+		t['singing steel'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 20 }
+		t['sunsilver'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10 }
+		t['silver'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10 }		-- This is a workaround because nobody says "alchemical silver" in their weapon names
+	--	t['viridium'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10 }		-- viridium weapons have half the hardness of their base weapon 
+		t['voidglass'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['whipwood'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 10, ['nWeaponHpBonus'] = 5 }
+		t['voidglass'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['wyroot'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }
 		
 		-- substances from Damaging Items list
-		t['glass'] = { ['nHardness'] = 1, ['nHpIn'] = 1 }
-		t['paper'] = { ['nHardness'] = 0, ['nHpIn'] = 2 }
-		t['cloth'] = { ['nHardness'] = 0, ['nHpIn'] = 2 }
-		t['fabric'] = { ['nHardness'] = 0, ['nHpIn'] = 2 }		-- Just in case
-		t['rope'] = { ['nHardness'] = 0, ['nHpIn'] = 2 }
-		t['ice'] = { ['nHardness'] = 0, ['nHpIn'] = 3 }
-		t['leather'] = { ['nHardness'] = 2, ['nHpIn'] = 5 }
-		t['hide'] = { ['nHardness'] = 2, ['nHpIn'] = 5 }
-		t['wood'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }
-		t['wooden'] = { ['nHardness'] = 5, ['nHpIn'] = 10 }		-- Just in case
-		t['stone'] = { ['nHardness'] = 8, ['nHpIn'] = 15 }
-		t['iron'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['steel'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }
-		t['metal'] = { ['nHardness'] = 10, ['nHpIn'] = 30 }		-- This is an oversimplification, but I think it's warranted
-		t['mithral'] = { ['nHardness'] = 15, ['nHpIn'] = 30 }
-		t['adamantine'] = { ['nHardness'] = 20, ['nHpIn'] = 40, ['nArmorHpMult'] = 1.33 }
+		t['glass'] = { ['nHardness'] = 1, ['nItemHpPerIn'] = 1 }
+		t['paper'] = { ['nHardness'] = 0, ['nItemHpPerIn'] = 2 }
+		t['cloth'] = { ['nHardness'] = 0, ['nItemHpPerIn'] = 2 }
+		t['fabric'] = { ['nHardness'] = 0, ['nItemHpPerIn'] = 2 }		-- Just in case
+		t['rope'] = { ['nHardness'] = 0, ['nItemHpPerIn'] = 2 }
+		t['ice'] = { ['nHardness'] = 0, ['nItemHpPerIn'] = 3 }
+		t['leather'] = { ['nHardness'] = 2, ['nItemHpPerIn'] = 5 }
+		t['hide'] = { ['nHardness'] = 2, ['nItemHpPerIn'] = 5 }
+		t['wood'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }
+		t['wooden'] = { ['nHardness'] = 5, ['nItemHpPerIn'] = 10 }		-- Just in case
+		t['stone'] = { ['nHardness'] = 8, ['nItemHpPerIn'] = 15 }
+		t['iron'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['steel'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }
+		t['metal'] = { ['nHardness'] = 10, ['nItemHpPerIn'] = 30 }		-- This is an oversimplification, but I think it's warranted
+		t['mithral'] = { ['nHardness'] = 15, ['nItemHpPerIn'] = 30 }
+		t['adamantine'] = { ['nHardness'] = 20, ['nItemHpPerIn'] = 40, ['nArmorHpMult'] = 1.33 }
 	end
 
 	if sK == 'items' then
