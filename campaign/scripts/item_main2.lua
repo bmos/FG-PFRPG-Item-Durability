0f352b84b22312496b29ab64155dbdf69a89a56f
Wil Thieme <wil.thieme@protonmail.com>
2020-12-01 12:24:33 -0500

    Update item_main2.lua

--- campaign/scripts/item_main2.lua
+++ campaign/scripts/item_main2.lua
@@ -102,15 +102,6 @@ function update()
 	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	
-	local bSection8 = false; -- Advanced Character Inventory Manager
-	local bACIMisLoaded = StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder');
-	if bACIMisLoaded then
-		if updateControl('damage', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl('damagetype', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl('critical', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl('range', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-	end
-	
 	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('checkpenalty', bReadOnly, bID and bArmor) then bSection4 = true; end
@@ -118,25 +109,6 @@ function update()
 	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('properties', bReadOnly, bID) then bSection4 = true; end
-	
---	This is compatibility for 'Enhanced Items' by Llisandur
-	local bPFRPGEILoaded = StringManager.contains(Extension.getExtensions(), 'PFRPG - Enhanced Items');
-	if bPFRPGEILoaded then
-		current_label.setVisible(false);
-		maxcharges.setVisible(false);
-		maxcharges_label.setVisible(false);
-		if updateControl('charge', bReadOnly, bID and (bWand or bStaff)) then
-			current_label.setVisible(true);
-			maxcharges.setVisible(true);
-			maxcharges.setReadOnly(bReadOnly);
-			maxcharges_label.setVisible(true);
-			bSection8 = true;
-		end
-		charge.setReadOnly(false);
-
-		if updateControl('equipslot', bReadOnly, bID and bWondrous) then bSection8 = true; end
-	end
---	End compatibility patch
 	
 	local bSection5 = false
 	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
@@ -170,17 +142,45 @@ function update()
 	end
 	--	End compatibility patch
 	
+	local bSection8 = false; -- Advanced Character Inventory Manager
+	local bACIMisLoaded = StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder');
+	if bACIMisLoaded then
+		if updateControl('damage', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('damagetype', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('critical', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('range', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+	end
+	
+--	This is compatibility for 'Enhanced Items' by Llisandur
+	local bPFRPGEILoaded = StringManager.contains(Extension.getExtensions(), 'PFRPG - Enhanced Items');
+	if bPFRPGEILoaded then
+		current_label.setVisible(false);
+		maxcharges.setVisible(false);
+		maxcharges_label.setVisible(false);
+		if updateControl('charge', bReadOnly, bID and (bWand or bStaff)) then
+			current_label.setVisible(true);
+			maxcharges.setVisible(true);
+			maxcharges.setReadOnly(bReadOnly);
+			maxcharges_label.setVisible(true);
+			bSection8 = true;
+		end
+		charge.setReadOnly(false);
+
+		if updateControl('equipslot', bReadOnly, bID and bWondrous) then bSection8 = true; end
+	end
+--	End compatibility patch
+	
 	divider.setVisible(bSection1 and bSection2);
 	divider2.setVisible((bSection1 or bSection2) and bSection3);
 	divider3.setVisible((bSection1 or bSection2 or bSection3) and bSection3a);
 	divider3a.setVisible((bSection1 or bSection2 or bSection3 or bSection3a) and bSection3b);
 	divider3b.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b) and bSection4);
-	divider4.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4) and bSection5);
-	divider5.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5) and bSection6);
+	divider4.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b or bSection4) and bSection5);
+	divider5.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b or bSection4 or bSection5) and bSection6);
 	if bPFRPGEILoaded and User.isHost() then
-		divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5 or bSection6) and bSection7);
+		divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b or bSection4 or bSection5 or bSection6) and bSection7);
 	end
 	if bACIMisLoaded then
-		divider8.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5 or bSection6 or bSection7) and bSection8);
+		divider8.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b or bSection4 or bSection5 or bSection6 or bSection7) and bSection8);
 	end
 end
