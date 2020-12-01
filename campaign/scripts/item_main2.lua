4ba53a53a5f662280c9a2eecfc8328888fbe8acb
Wil Thieme <wil.thieme@protonmail.com>
2020-12-01 12:14:32 -0500

    item saving throws and ui work

--- campaign/scripts/item_main2.lua
+++ campaign/scripts/item_main2.lua
@@ -3,120 +3,113 @@
 --
 
 function onInit()
-	update()
+	update();
 end
 
 function VisDataCleared()
-	update()
+	update();
 end
 
 function InvisDataAdded()
-	update()
+	update();
 end
 
 function updateControl(sControl, bReadOnly, bID)
 	if not self[sControl] then
-		return false
+		return false;
 	end
 		
 	if not bID then
-		return self[sControl].update(bReadOnly, true)
+		return self[sControl].update(bReadOnly, true);
 	end
 	
-	return self[sControl].update(bReadOnly)
+	return self[sControl].update(bReadOnly);
+end
+
+function getItemType()
+	local bWeapon, bArmor, bShield, bWand, bStaff, bWondrous;
+	local sType = string.lower(type.getValue());
+	if sType:match('weapon') then
+		bWeapon = true;
+	end
+	if sType:match('armor') then
+		bArmor = true;
+	end
+	if sType:match('wand') then
+		bWand = true;
+	end
+	if sType:match('staff') then
+		bStaff = true;
+	end
+	if sType:match('wondrous item') then
+		bWondrous = true;
+	end
+	if sType:match('shield') then
+		bShield = true;
+	end
+	return bWeapon, bArmor, bShield, bWand, bStaff, bWondrous;
 end
 
 function update()
-	local nodeRecord = getDatabaseNode()
-	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord)
-	local bID, bOptionID = ItemManager.getIDState(nodeRecord)
-
-	local sType = string.lower(type.getValue())
+	local nodeRecord = getDatabaseNode();
+	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
+	local bID = LibraryData.getIDState("item", nodeRecord);
 	
-	local bWeapon, bArmor, bShield, bWand, bStaff, bWondrous
+	local bWeapon, bArmor, bShield, bWand, bStaff, bWondrous = getItemType();
 
-	if sType:match('weapon') then
-		bWeapon = true
+	local bSection1 = false;
+	if User.isHost() then
+		if updateControl("nonid_name", bReadOnly, true) then bSection1 = true; end;
 	else
-		bWeapon = false
+		updateControl("nonid_name", false);
 	end
-	if sType:match('armor') then
-		bArmor = true
+	if (User.isHost() or not bID) then
+		if updateControl("nonidentified", bReadOnly, true) then bSection1 = true; end;
 	else
-		bArmor = false
+		updateControl("nonidentified", false);
 	end
-	if sType:match('wand') then
-		bWand = true
-	else
-		bWand = false
-	end
-	if sType:match('staff') then
-		bStaff = true
-	else
-		bStaff = false
-	end
-	if sType:match('wondrous item') then
-		bWondrous = true
-	else
-		bWondrous = false
-	end
-	if sType:match('shield') then
-		bShield = true
-	else
-		bShield = false
-	end
-
-	local bSection1 = false
-	if bOptionID and User.isHost() then
-		if updateControl('nonid_name', bReadOnly, true) then bSection1 = true; end
-	else
-		updateControl('nonid_name', false)
-	end
-	if bOptionID and (User.isHost() or not bID) then
-		if updateControl('nonidentified', bReadOnly, true) then bSection1 = true; end
-	else
-		updateControl('nonidentified', false)
-	end
-
-	local bSection2 = false
+	
+	local bSection2 = false;
 	if updateControl('type', bReadOnly, bID) then bSection2 = true; end
 	if updateControl('subtype', bReadOnly, bID) then bSection2 = true; end
 	
-	local bSection2a = false
-	if updateControl('hardness', bReadOnly, bID) then bSection2a = true; end
-	button_rebuildhhp.setVisible(updateControl('hardness', bReadOnly, bID))
-	if updateControl('hitpoints', bReadOnly, bID) then bSection2a = true; end
-	if updateControl('itemdamage', bReadOnly, bID) then bSection2a = true; end
-	if updateControl('hitpoints', bReadOnly, bID) then itemdamage.setVisible(true); itemdamage_label.setVisible(true) end
-	itemdamage.setReadOnly(false)
-		
-	local bSection3 = false
+	local bSection3 = false;
 	if updateControl('cost', bReadOnly, bID) then bSection3 = true; end
 	if updateControl('weight', bReadOnly, bID) then bSection3 = true; end
-	if updateControl('substance', bReadOnly, bID) then bSection3 = true; end
-	button_rebuildattributes.setVisible(updateControl('substance', bReadOnly, bID))
-	if bArmor then -- Don't show thickness if item is armor
-		thickness.setVisible(false); thickness_label.setVisible(false)
-	elseif
-		updateControl('thickness', bReadOnly, bID) then thickness.setVisible(true); thickness_label.setVisible(true); bSection3 = true
-	end
-	if updateControl('size', bReadOnly, bID) then bSection3 = true; end
-
---	This is compatibility for 'Advanced Character Inventory Manager' by rmilmine
-	local bSection8 = false
-	if StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder') then
+	
+	local bSection3a = false; -- Item Durability
+	if updateControl('hardness', bReadOnly, bID) then bSection3a = true; end
+	if updateControl('hitpoints', bReadOnly, bID) then bSection3a = true; end
+	if updateControl('itemdamage', bReadOnly, bID) then bSection3a = true; end
+	item_durability_label.setVisible(bSection3a); -- all-or-nothing hide/show of section
+	hardness.setVisible(bSection3a); hardness_label.setVisible(bSection3a);
+	hitpoints.setVisible(bSection3a); hitpoints_label.setVisible(bSection3a);
+	itemdamage.setVisible(bSection3a); itemdamage_label.setVisible(bSection3a); itemdamage.setReadOnly(false);
+	button_rebuildhhp.setVisible(bSection3a);
+	
+	local bSection3b = false; -- Item Durability
+	if updateControl('size', bReadOnly, bID) then bSection3b = true; end
+	if updateControl('substance', bReadOnly, bID) then bSection3b = true; end
+	if updateControl('thickness', bReadOnly, bID and not bArmor) then bSection3b = true end
+	button_rebuildattributes.setVisible(bSection3b); -- all-or-nothing hide/show of section
+	size.setVisible(bSection3b); size_label.setVisible(bSection3b);
+	substance.setVisible(bSection3b); substance_label.setVisible(bSection3b);
+	thickness.setVisible(bSection3b); thickness_label.setVisible(bSection3b);
+	
+	local bSection4 = false;
+	if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	
+	local bSection8 = false; -- Advanced Character Inventory Manager
+	local bACIMisLoaded = StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder');
+	if bACIMisLoaded then
 		if updateControl('damage', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 		if updateControl('damagetype', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 		if updateControl('critical', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 		if updateControl('range', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 	end
---	End compatibility patch
-
-	local bSection4 = false
-	if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	
 	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true; end
@@ -124,71 +117,70 @@ function update()
 	if updateControl('spellfailure', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true; end
-
+	if updateControl('properties', bReadOnly, bID) then bSection4 = true; end
+	
 --	This is compatibility for 'Enhanced Items' by Llisandur
-	local bPFRPGEILoaded = StringManager.contains(Extension.getExtensions(), 'PFRPG - Enhanced Items')
+	local bPFRPGEILoaded = StringManager.contains(Extension.getExtensions(), 'PFRPG - Enhanced Items');
 	if bPFRPGEILoaded then
-		current_label.setVisible(false)
-		maxcharges.setVisible(false)
-		maxcharges_label.setVisible(false)
+		current_label.setVisible(false);
+		maxcharges.setVisible(false);
+		maxcharges_label.setVisible(false);
 		if updateControl('charge', bReadOnly, bID and (bWand or bStaff)) then
-			current_label.setVisible(true)
-			maxcharges.setVisible(true)
-			maxcharges.setReadOnly(bReadOnly)
-			maxcharges_label.setVisible(true)
-			bSection8 = true
+			current_label.setVisible(true);
+			maxcharges.setVisible(true);
+			maxcharges.setReadOnly(bReadOnly);
+			maxcharges_label.setVisible(true);
+			bSection8 = true;
 		end
-		charge.setReadOnly(false)
+		charge.setReadOnly(false);
 
 		if updateControl('equipslot', bReadOnly, bID and bWondrous) then bSection8 = true; end
 	end
 --	End compatibility patch
-
-	if updateControl('properties', bReadOnly, bID and (bArmor or bWeapon)) then bSection4 = true; end -- shows the divider between cost/size and hardness/damage
 	
 	local bSection5 = false
 	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
 	if updateControl('aura', bReadOnly, bID) then bSection5 = true; end
 	if updateControl('cl', bReadOnly, bID) then bSection5 = true; end
 	if updateControl('prerequisites', bReadOnly, bID) then bSection5 = true; end
+	fortitudesave.setVisible(bSection5); fortitudesave_label.setVisible(bSection5);
+	reflexsave.setVisible(bSection5); reflexsave_label.setVisible(bSection5);
+	willsave.setVisible(bSection5); willsave_label.setVisible(bSection5);
+	item_saves_label.setVisible(bSection5);
 	
-	local bSection6 = bID
-	description.setVisible(bID)
-	description.setReadOnly(bReadOnly)
-
+	local bSection6 = bID;
+	description.setVisible(bID);
+	description.setReadOnly(bReadOnly);
+	
 --	This is compatibility for 'Customised Item Generator for 3.5E and Pathfinder' by rmilmine
-	if bArmor and StringManager.contains(Extension.getExtensions(), 'Customised Item Generator for 3.5E and Pathfinder') then bSection6 = true; end
+	--if bArmor and StringManager.contains(Extension.getExtensions(), 'Customized Item Generator for 3.5E and Pathfinder') then bSection6 = true; end
 --	End compatibility patch
-
---	This is compatibility for 'Enhanced Items' by Llisandur
+	
+	--	This is compatibility for 'Enhanced Items' by Llisandur
+	local bSection7 = false
 	if bPFRPGEILoaded then
-		local bSection7 = false
-		updateControl('sourcebook', bReadOnly, bID)
-		divider6.setVisible(false)
-		gmonly_label.setVisible(false)
-		gmonly.setVisible(false)
-		if bOptionID and User.isHost() then
+		updateControl('sourcebook', bReadOnly, bID);
+		gmonly_label.setVisible(false);
+		gmonly.setVisible(false);
+		if User.isHost() then
 			if updateControl('gmonly', bReadOnly, true) then bSection7 = true; end
 		else
-			updateControl('gmonly', bReadOnly, false)
-		end
-		if User.isHost() then 
-			divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection4 or bSection5) and bSection7)
+			updateControl('gmonly', bReadOnly, false);
 		end
 	end
---	End compatibility patch
-
-	divider.setVisible(bSection1 and bSection2)
-	divider2.setVisible((bSection1 or bSection2) and bSection2a)
-	divider2a.setVisible((bSection2a or bSection4) and bSection3)
-
---	This is compatibility for 'Advanced Character Inventory Manager' by rmilmine
-	if StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder') then
-		divider8.setVisible((bSection1 or bSection2 or bSection3 or bSection4) and bSection8)
+	--	End compatibility patch
+	
+	divider.setVisible(bSection1 and bSection2);
+	divider2.setVisible((bSection1 or bSection2) and bSection3);
+	divider3.setVisible((bSection1 or bSection2 or bSection3) and bSection3a);
+	divider3a.setVisible((bSection1 or bSection2 or bSection3 or bSection3a) and bSection3b);
+	divider3b.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection3b) and bSection4);
+	divider4.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4) and bSection5);
+	divider5.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5) and bSection6);
+	if bPFRPGEILoaded and User.isHost() then
+		divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5 or bSection6) and bSection7);
 	end
---	End compatibility patch
-
-	divider3.setVisible((bSection1 or bSection2 or bSection2a or bSection3) and bSection4)
-	divider4.setVisible((bSection1 or bSection2 or bSection2a or bSection3 or bSection4) and bSection5)
-	divider5.setVisible((bSection1 or bSection2 or bSection2a or bSection3 or bSection4 or bSection5) and bSection6)
+	if bACIMisLoaded then
+		divider8.setVisible((bSection1 or bSection2 or bSection3 or bSection3a or bSection4 or bSection5 or bSection6 or bSection7) and bSection8);
+	end
 end
