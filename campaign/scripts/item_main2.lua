e47877d3906898e53bfcbd9e7574934893ce1c24
Wil Thieme <wil.thieme@protonmail.com>
2021-01-04 18:24:38 -0500

    clear empty lines

--- campaign/scripts/item_main2.lua
+++ campaign/scripts/item_main2.lua
@@ -1,4 +1,4 @@
--- 
+--
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
@@ -18,11 +18,11 @@ function updateControl(sControl, bReadOnly, bID)
 	if not self[sControl] then
 		return false;
 	end
-		
+
 	if not bID then
 		return self[sControl].update(bReadOnly, true);
 	end
-	
+
 	return self[sControl].update(bReadOnly);
 end
 
@@ -55,7 +55,7 @@ function update()
 	local nodeRecord = getDatabaseNode();
 	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
 	local bID = LibraryData.getIDState("item", nodeRecord);
-	
+
 	local bWeapon, bArmor, bShield, bWand, bStaff, bWondrous = getItemType();
 
 	local bSection1 = false;
@@ -69,15 +69,15 @@ function update()
 	else
 		updateControl("nonidentified", false);
 	end
-	
+
 	local bSection2 = false;
 	if updateControl('type', bReadOnly, bID) then bSection2 = true; end
 	if updateControl('subtype', bReadOnly, bID) then bSection2 = true; end
-	
+
 	local bSection3 = false;
 	if updateControl('cost', bReadOnly, bID) then bSection3 = true; end
 	if updateControl('weight', bReadOnly, bID) then bSection3 = true; end
-	
+
 	local bSection3a = false; -- Item Durability
 	if updateControl('hardness', bReadOnly, bID) then bSection3a = true; end
 	if updateControl('hitpoints', bReadOnly, bID) then bSection3a = true; end
@@ -87,7 +87,7 @@ function update()
 	hitpoints.setVisible(bSection3a); hitpoints_label.setVisible(bSection3a);
 	itemdamage.setVisible(bSection3a); itemdamage_label.setVisible(bSection3a); itemdamage.setReadOnly(false);
 	button_rebuildhhp.setVisible(bSection3a);
-	
+
 	local bSection3b = false; -- Item Durability
 	if updateControl('size', bReadOnly, bID) then bSection3b = true; end
 	if updateControl('substance', bReadOnly, bID) then bSection3b = true; end
@@ -96,13 +96,13 @@ function update()
 	size.setVisible(bSection3b); size_label.setVisible(bSection3b);
 	substance.setVisible(bSection3b); substance_label.setVisible(bSection3b);
 	thickness.setVisible(bSection3b and not bArmor); thickness_label.setVisible(bSection3b and not bArmor);
-	
+
 	local bSection4 = false;
 	if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
-	
+
 	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('checkpenalty', bReadOnly, bID and bArmor) then bSection4 = true; end
@@ -110,7 +110,7 @@ function update()
 	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true; end
 	if updateControl('properties', bReadOnly, bID) then bSection4 = true; end
-	
+
 	local bSection5 = false
 	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
 	if updateControl('aura', bReadOnly, bID) then bSection5 = true; end
@@ -121,11 +121,11 @@ function update()
 	reflexsave.setVisible(bCL); reflexsave_label.setVisible(bCL);
 	willsave.setVisible(bCL); willsave_label.setVisible(bCL);
 	item_saves_label.setVisible(bCL);
-	
+
 	local bSection6 = bID;
 	description.setVisible(bID);
 	description.setReadOnly(bReadOnly);
-		
+
 	--	This is compatibility for 'Enhanced Items' by Llisandur
 	local bSection7 = false;
 	if bPFRPGEILoaded then
@@ -139,7 +139,7 @@ function update()
 		end
 	end
 	--	End compatibility patch
-	
+
 	local bSection8 = false; -- Advanced Character Inventory Manager
 	local bACIMisLoaded = StringManager.contains(Extension.getExtensions(), 'Advanced Character Inventory Manager for 3.5E and Pathfinder');
 	if bACIMisLoaded then
@@ -148,7 +148,7 @@ function update()
 		if updateControl('critical', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 		if updateControl('range', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 	end
-	
+
 --	This is compatibility for 'Enhanced Items' by Llisandur
 	local bPFRPGEILoaded = StringManager.contains(Extension.getExtensions(), 'PFRPG - Enhanced Items');
 	if bPFRPGEILoaded then
@@ -167,7 +167,7 @@ function update()
 		updateControl('equipslot', bReadOnly, bID and bWondrous)
 	end
 --	End compatibility patch
-	
+
 	divider.setVisible(bSection1 and bSection2);
 	divider2.setVisible((bSection1 or bSection2) and bSection3);
 	divider3.setVisible((bSection1 or bSection2 or bSection3) and bSection3a);
